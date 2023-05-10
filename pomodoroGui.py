3787e98a86ae566cc0fbce9468fc72dfa76f33f6
Bedirhan Alp Arslan <89816332+balpars@users.noreply.github.com>
2023-05-10 23:36:40 +0300

    added alarm when countown finishes

--- pomodoroGui.py
+++ pomodoroGui.py
@@ -1,7 +1,8 @@
 from tkinter import *
-import time
+import pygame
 
-def countdown(count):
+
+def countdown(count=25*60):
     # change text in label
     time_str = time_string(count)
     countdown_label['text'] = time_str
@@ -9,8 +10,17 @@ def countdown(count):
     if count > 0:
         # call countdown again after 1000ms (1s)
         root.after(1000, countdown, count-1)
+    if count <= 0:
+        play()
+
+def onClick():
+    if countdown_label['text'] != initial_text:
+        return
+    else:
+        countdown(25*60)
 
 def time_string(seconds:int) -> str:
+
     m = seconds // 60
     s = seconds % 60
 
@@ -24,19 +34,25 @@ def time_string(seconds:int) -> str:
         time_str += "0"
     time_str += str(s)
 
-
-
     return time_str
+
+pygame.mixer.init() # initialise the pygame
+
+def play():
+    pygame.mixer.music.load("alarm.mp3")
+    pygame.mixer.music.play(loops=0)
 
 root = Tk()
 root.title('Pomodoro')
-root.geometry("600x400")
+root.geometry("350x350")
+root.config(bg="black")
 
+initial_text = "Press"
 
 # Label
 countdown_label = Label(
-    root, 
-    text="", 
+    root,
+    text= initial_text,
     font=("Helvetica", 48),
     fg="green",
     bg="black"
@@ -46,6 +62,17 @@ countdown_label.pack(
     pady=20
 )
 
-countdown(25*60)
+start_button = Button(
+    root, 
+    bg="green",
+    width= 10,
+    height= 2,
+    command=onClick
+    )
+start_button.pack(
+
+    pady= 80
+)
+
 root.mainloop()
 
---
+++ test.py
@@ -0,0 +1,6 @@
+def OnPressed(event):
+    print('Hello')
+def OnHover(event):
+    But.config(bg='red', fg='white')
+def OnLeave(event):
+    But.config(bg='white', fg='black')
